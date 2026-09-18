c2d152698e5fddd44bc48548b8da2cc27e2f2899
Michael Palmas <mky180@Gmail.com>
2026-09-18 11:30:09 -0700

    fix: move RC species IDs beyond CFRU range

--- tests/test_rc_sprite_assets.py
+++ tests/test_rc_sprite_assets.py
@@ -33,14 +33,14 @@ class ReleaseCandidateSpriteAssetTest(unittest.TestCase):
             self.assertEqual(
                 {p.relative_to(out).as_posix() for p in written},
                 {
-                    "graphics/frontspr/gFrontSprite1268RCTartrek.png",
-                    "graphics/backspr/gBackShinySprite1268RCTartrek.png",
-                    "graphics/pokeicon/gIconSprite1268RCTartrek.png",
+                    "graphics/frontspr/gFrontSprite1294RCTartrek.png",
+                    "graphics/backspr/gBackShinySprite1294RCTartrek.png",
+                    "graphics/pokeicon/gIconSprite1294RCTartrek.png",
                 },
             )
-            self.assertEqual(read_png_header(out / "graphics/frontspr/gFrontSprite1268RCTartrek.png"), (64, 64, 4, 3))
-            self.assertEqual(read_png_header(out / "graphics/backspr/gBackShinySprite1268RCTartrek.png"), (64, 64, 4, 3))
-            self.assertEqual(read_png_header(out / "graphics/pokeicon/gIconSprite1268RCTartrek.png"), (32, 64, 4, 3))
+            self.assertEqual(read_png_header(out / "graphics/frontspr/gFrontSprite1294RCTartrek.png"), (64, 64, 4, 3))
+            self.assertEqual(read_png_header(out / "graphics/backspr/gBackShinySprite1294RCTartrek.png"), (64, 64, 4, 3))
+            self.assertEqual(read_png_header(out / "graphics/pokeicon/gIconSprite1294RCTartrek.png"), (32, 64, 4, 3))
 
 
 if __name__ == "__main__":
