ba2e7e488db8c99b9ccebe1c8de777374783a0dd
Michael Palmas <mky180@Gmail.com>
2026-09-26 09:21:30 -0700

    fix: compile RC National Dex mappings

--- include/pokedex.h
+++ include/pokedex.h
@@ -1008,8 +1008,13 @@ enum
 #define NATIONAL_DEX_SPECTRIER 897
 #define NATIONAL_DEX_CALYREX 898
 
-#define FINAL_DEX_ENTRY NATIONAL_DEX_CALYREX //Not +1 b/c used like this for some asm
+#define FINAL_DEX_ENTRY NATIONAL_DEX_RC_MISTRILLO //Not +1 b/c used like this for some asm
 #define NATIONAL_DEX_COUNT FINAL_DEX_ENTRY + 1
+
+extern const u8 DEX_ENTRY_RC_TARTREK[];
+extern const u8 DEX_ENTRY_RC_FROBYTE[];
+extern const u8 DEX_ENTRY_RC_EMBERFOX[];
+extern const u8 DEX_ENTRY_RC_MISTRILLO[];
 
 extern const u8 DEX_ENTRY_TURTWIG[];
 extern const u8 DEX_ENTRY_GROTLE[];
